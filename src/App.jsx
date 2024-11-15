07fdb144df56d196f46eaa1d9c0d2615d7cd9323
[shashikant] <[shashikant.mane801@gmail.com]>
2024-11-15 13:52:36 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import InvoiceForm from './components/InvoiceForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <InvoiceForm />
+    </div>
+  );
+}
+
+export default App;
